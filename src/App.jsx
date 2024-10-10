e933c183533f4cd6c9d03fc7dd849af61b2b3be9
Federico Holc <federicoholc@gmail.com>
2024-10-10 13:03:41 -0300

    Refactor App component to use user ID for room creation and enhance UI styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,50 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [roomId, setRoomId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [roomUrl, setRoomUrl] = useState("");
   const navigate = useNavigate();
 
-  const roomUrl = `${window.location.origin}/vl/${roomId}`;
+  //random 7 digitos
 
   return (
     <div className="app">
       <h1>Hola</h1>
 
-      <label>Ingrese el nombre de la sala: </label>
+      <label>Ingrese su número/código de identificación</label>
       <input
         type="text"
         id="roomId"
-        value={roomId}
+        value={userId}
         onChange={(e) => {
-          setRoomId(e.target.value);
+          setUserId(e.target.value);
         }}
       />
+      <button
+        onClick={() => {
+          if (userId === "") {
+            alert("Debe ingresar un número/código de identificación");
+            return;
+          }
+          const randomNumber = Math.floor(1000000 + Math.random() * 9000000);
+          setRoomUrl(`${window.location.origin}/vl/${userId}-${randomNumber}`);
+        }}
+      >
+        Solicitar Videollamada
+      </button>
+      {roomUrl && (
+        <div>
+          <p>
+            {" "}
+            Para realizar la videollamada ingrese al siguiente enlace y aguarde
+            a ser atendido/a:{" "}
+          </p>
 
-      <div>
-        Enlace a la sala:{" "}
-        <a href={`/vl/${roomId}`}>{roomId !== "" ? roomUrl : ""}</a>
-      </div>
-      <button onClick={() => navigate(`/vl/${roomId}`)}>Ir a la sala</button>
+          <a href={`/vl/${userId}`}>{userId !== "" ? roomUrl : ""}</a>
+        </div>
+      )}
+      {/* 
+      <button onClick={() => navigate(`/vl/${userId}`)}>Ir a la sala</button> */}
     </div>
   );
 }
